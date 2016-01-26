8f92a91c46e086656d0f3f1eccfa0a279bacec91
Fabian Vogelsteller <fabian@frozeman.de>
2016-01-26 16:44:38 +0100

    added test files

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # electron-curstom-protocol-realtive-urls-repro
 Test repro for https://github.com/atom/electron/issues/1747#issuecomment-174792471
+
+
+To run the repro, you need to have Meteor.js installed. If not install meteor as follows:
+
+     curl https://install.meteor.com/ | sh
+
+Then start the test app by running:
+
+    $ cd repro/myapp
+    $ meteor --production
+
+In another terminal start the electron app:
+
+   $ cd repro/
+   $ electron .
+
+To test the difference, run the meteor app without the `--production` flag and then restart electron.
+You may need to increase the number after the `?` in line 77 of the `main.js`, to prevent caching.
---
+++ main.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+
+var mainWindow = null;
+
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  const protocol = require('protocol');
+  const fs = require('fs');
+
+  // add custom protocol
+  protocol.interceptHttpProtocol('http', function(request, callback) {
+
+      var call = {
+          // rewrite all 
+          url: (request.url.indexOf('http://interface') !== -1)
+            ? 'http://localhost:4000/' + request.url.replace('http://interface/','').replace('http://interface','').replace('http:///','').replace('http://','')
+            : request.url,
+          method: request.method,
+          referrer: request.referrer,
+          session: null
+      };
+
+      console.log(request.url, ' -> ', call.url);
+
+      callback(call);
+
+  }, function (error) {
+    if (error)
+      console.error('Failed to register protocol', error)
+  });
+
+ // protocol.registerStandardSchemes(['myprot']);
+
+
+  // BUFFER protocol works!
+  // protocol.registerBufferProtocol('myprot', function(request, callback) {
+
+  //     console.log('Loading '+ '/Users/frozeman/Sites/_ethereum/meteor-dapp-wallet/build' + request.url.replace('myprot://interface',''));
+
+  //     fs.readFile('/Users/frozeman/Sites/_ethereum/meteor-dapp-wallet/build' + request.url.replace('myprot://interface',''), function (err, data) {
+  //         if (err) throw err;
+
+  //         var type = 'text/html';
+
+  //         type = (request.url.indexOf('.css') !== -1) ? 'text/css' : type;
+  //         type = (request.url.indexOf('.js') !== -1) ? 'text/javascript' : type;
+    
+  //         callback({
+  //           mimeType: type,
+  //           data: data
+  //         });
+  //     });
+
+  // }, function (error) {
+  //   if (error)
+  //     console.error('Failed to register protocol')
+  // });
+
+
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1200, height: 800});
+  // webPreferences: {
+  //   nodeIntegration: false,
+  //   webSecurity: false,
+  //   allowRunningInsecureContent: true
+  // }
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('http://interface/?65432');
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+
+});
